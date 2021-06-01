ff726b7df2cbb8719048194cee069fad9f1b5876
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-06-01 11:46:00 +0530

    Add files via upload

---
+++ lists-tuples.py
@@ -0,0 +1,27 @@
+#program to store seven fruits in a list entered by the user.
+
+fruits = ["apple", "orange", "kiwi", "mango", "strawberry", "banana", "melon"]
+print(fruits)
+
+#program to accept marks of 6 students and display them in a sorted manner
+
+marks_1 = float(input("Student 1 "))
+marks_2 = float(input("Student 2 "))
+marks_3 = float(input("Student 3 "))
+marks_4 = float(input("Student 4 "))
+marks_5 = float(input("Student 5 "))
+marks_6 = float(input("Student 6 "))
+
+marks = [marks_1, marks_2, marks_3, marks_4, marks_5, marks_6]
+marks.sort()
+print(marks)
+
+# program to sum a list with 4 numbers.
+
+a = [1, 5, 8, 10]
+print(sum(a))
+
+#a program to count the number of zeros in the following tuple
+tup_le = (7, 0, 8, 0, 0, 9)
+print(tup_le.count(0))
+
